f8d6b55b0715b78acc225195c29d768127d59c56
Frank Lamantia <frank@boldpenguin.com>
2020-12-06 22:53:06 -0500

    Refactoring to use a group class for cleanliness

--- lib/declaration_form.rb
+++ lib/declaration_form.rb
@@ -1,31 +1,46 @@
 class DeclarationForm
   def load(input)
     @lines = input.split(/\n/)
+    @groups ||= []
+    until @lines.empty?
+      @groups << next_group
+    end
   end
 
   def sum
-    sum = 0
-    until @lines.empty?
-      group = next_group()
-      size = group.split("\n").size
+    @groups.sum(&:count)
+  end
 
-      unique = group.gsub(/\s+/, "").split("").uniq
-
-      unique.each do |character|
-        sum +=1 if group.count(character) == size
-      end
+  class Group
+    def initialize
+      @members = []
     end
-    sum
+
+    def size
+      @members.size
+    end
+
+    def add(line)
+      @members << line
+    end
+
+    def count
+      letters = @members.flatten.join("")
+      letters.split("").uniq.count { |character|
+        # If every member had this answer...
+        letters.count(character) == size
+      }
+    end
   end
 
   private
 
   def next_group
-    answers = []
-    while !@lines.empty? && !@lines.first.empty?
-      answers << @lines.shift
+    group = Group.new
+    until @lines.empty? || @lines.first.empty?
+      group.add(@lines.shift)
     end
     @lines.shift unless @lines.empty?
-    answers.join("\n")
+    group
   end
 end
--- spec/declaration_form_spec.rb
+++ spec/declaration_form_spec.rb
@@ -4,6 +4,25 @@ describe DeclarationForm do
   let(:form) {
     DeclarationForm.new
   }
+  describe "Group" do
+    describe "#add" do
+      it "should add members" do
+        group = DeclarationForm::Group.new
+        expect(group.size).to eq 0
+        group.add("abc")
+        expect(group.size).to eq 1
+      end
+    end
+    describe "#sum" do
+      it "should calculate the sum" do
+        group = DeclarationForm::Group.new
+        group.add("abc")
+        expect(group.count).to eq 3
+        group.add("a")
+        expect(group.count).to eq 1
+      end
+    end
+  end
 
   it "should handle the sample input" do
     input = File.read("spec/inputs/day-6/sample.txt")
